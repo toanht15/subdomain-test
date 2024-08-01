7cb776f8aaedf4181aaca679cf5e332bee1df56e
hoangthetoan <toanht15@gmail.com>
2024-08-01 16:44:08 +0700

    change title

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main className="bg-sky-300 h-screen flex flex-col items-center justify-center">
+    <main className="bg-gradient-to-r from-green-300 via-blue-300 to-pink-300 h-screen flex flex-col items-center justify-center">
       <p className="text-4xl text-emerald-600 font-medium">Subdomain test</p>
     </main>
   );
